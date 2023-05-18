4fbd51fc455f47562abedf24a4af6c987737b7cb
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-18 19:00:07 -0300

    fix: successfully added lint-staged and conventional commits

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,7 +3,7 @@ import { ChatBox, SideMenu } from '../components';
 // import styles from './app.module.css';
 const App = () => {
   return (
-    <div className="flex h-screen flex-row bg-gray-500">
+    <div className="flex h-screen flex-row bg-gray-400">
       <SideMenu />
 
       <section>
