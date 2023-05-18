c09865147934d736e8e587ccf0e36301075223e8
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-18 18:32:25 -0300

    fix: fixing commitlintrc settings and set it up
    adding commit lint setup
    
    fixing commit lint config
    
    fixing package json
    
    adding  commit lint rc
    
    testing commit hook
    
    testing remove deps

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,15 +1,16 @@
-import { ChatBox, SideMenu } from "../components";
+import { ChatBox, SideMenu } from '../components';
 
 // import styles from './app.module.css';
 const App = () => {
   return (
-    <div className="flex flex-row h-screen bg-gray-400">
+    <div className="flex h-screen flex-row bg-gray-500">
       <SideMenu />
+
       <section>
         <ChatBox />
       </section>
     </div>
   );
-}
+};
 
 export default App;
