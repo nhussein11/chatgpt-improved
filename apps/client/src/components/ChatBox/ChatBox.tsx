c470f7478a5f783aaa5d4700343e116d90f43f9f
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-19 10:56:57 -0300

    style: chat box fixing styles

--- apps/client/src/components/ChatBox/ChatBox.tsx
+++ apps/client/src/components/ChatBox/ChatBox.tsx
@@ -5,7 +5,7 @@ const ChatBox = () => {
   return (
     <div className="flex h-full w-full flex-col">
       <h1 className="mt-5 text-2xl font-bold text-white">ChatBox</h1>
-      <div className="flex-grow">
+      <div className="border-red flex-grow border-2">
         <ChatMessages />
       </div>
       <ChatPrompt />
