4988f06f6f92031fedd3439a987b560de656f16b
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-19 10:57:25 -0300

    style: fixing chat box styles and removing border

--- apps/client/src/components/ChatBox/ChatBox.tsx
+++ apps/client/src/components/ChatBox/ChatBox.tsx
@@ -4,8 +4,7 @@ import ChatPrompt from '../ChatPrompt/ChatPrompt';
 const ChatBox = () => {
   return (
     <div className="flex h-full w-full flex-col">
-      <h1 className="mt-5 text-2xl font-bold text-white">ChatBox</h1>
-      <div className="border-red flex-grow border-2">
+      <div className="flex-grow">
         <ChatMessages />
       </div>
       <ChatPrompt />
