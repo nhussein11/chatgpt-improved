8bff3d1e2dd48930f4340c8bf482d71ad2e14785
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-19 15:13:37 -0300

    fix: updating express app with new endpoint

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,17 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
-
+import { callApi } from '.';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (_req, res) => {
   res.send({ message: 'Welcome to server!' });
+});
+
+app.post('/api/create-completion', async (_req, res) => {
+  const data = await callApi();
+  res.send({data});
 });
 
 const port = process.env.PORT || 3000;
