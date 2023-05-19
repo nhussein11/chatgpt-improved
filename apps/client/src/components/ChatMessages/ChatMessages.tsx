0a8d1ee2563997672521efbb74c3607f201b1980
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-18 23:37:11 -0300

    feat: adding Chat Messages component

---
+++ apps/client/src/components/ChatMessages/ChatMessages.tsx
@@ -0,0 +1,12 @@
+const ChatMessages = () => {
+  return (
+    <div className="flex flex-grow flex-col-reverse overflow-y-auto">
+      <div className="flex flex-col-reverse">
+        <h3 className="text-2xl font-bold text-white">ChatMessages</h3>
+        <h3 className="text-2xl font-bold text-white">ChatMessages</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ChatMessages;
