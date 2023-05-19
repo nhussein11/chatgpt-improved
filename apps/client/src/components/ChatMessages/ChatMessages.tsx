c58fae69c964bc289b8bbfedfdce14f25b706053
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-19 10:57:46 -0300

    style: fixing drafted styles on chat messages

--- apps/client/src/components/ChatMessages/ChatMessages.tsx
+++ apps/client/src/components/ChatMessages/ChatMessages.tsx
@@ -1,9 +1,15 @@
+import { RxAvatar } from 'react-icons/rx';
+
 const ChatMessages = () => {
   return (
-    <div className="flex flex-grow flex-col-reverse overflow-y-auto">
-      <div className="flex flex-col-reverse">
-        <h3 className="text-2xl font-bold text-white">ChatMessages</h3>
-        <h3 className="text-2xl font-bold text-white">ChatMessages</h3>
+    <div className="flex h-full w-full flex-grow items-center justify-center overflow-y-auto">
+      <div className="flex h-24 w-3/4 flex-row items-center rounded-md border border-current bg-zinc-700 p-3 text-white">
+        <div className="flex h-full items-center ">
+          <RxAvatar className="justify-start text-3xl" />
+        </div>
+        <div className="flex h-full w-full items-center justify-center text-xl">
+          Message
+        </div>
       </div>
     </div>
   );
