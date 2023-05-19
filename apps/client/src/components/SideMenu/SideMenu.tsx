7d7639a1cc4ba4de14e6a7d221a6aa29a586073e
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-18 23:37:38 -0300

    fix: adding button and fixing styles to side menu

--- apps/client/src/components/SideMenu/SideMenu.tsx
+++ apps/client/src/components/SideMenu/SideMenu.tsx
@@ -1,12 +1,19 @@
 import styles from './sideMenu.module.css';
+import { HiOutlinePlus } from 'react-icons/hi';
 
 const SideMenu = () => {
   return (
     <>
-      <aside
-        className={`absolute top-0 left-0 h-full bg-gray-700 ${styles.menu}`}
-      >
-        <h1 className="mx-12 my-8 text-2xl font-bold text-white">Chat GPT</h1>
+      <aside className={`h-full bg-neutral-800 ${styles.menu}`}>
+        <h2 className="mx-12 my-8 text-2xl font-bold text-center text-white">Chat GPT</h2>
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center justify-center">
+            <button className="text-md mx-3 flex w-full flex-row items-center justify-center rounded-lg bg-neutral-700 py-2 text-center font-medium text-white hover:bg-neutral-600 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <HiOutlinePlus className="mr-2 text-xl" />
+              New Chat
+            </button>
+          </div>
+        </div>
       </aside>
     </>
   );
