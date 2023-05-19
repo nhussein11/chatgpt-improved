b61fcc26bcb73ce35152575fbf4a06b7d9e86e98
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-18 23:21:59 -0300

    style: app background and size

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,9 +3,9 @@ import { ChatBox, SideMenu } from '../components';
 // import styles from './app.module.css';
 const App = () => {
   return (
-    <div className="flex h-screen flex-row bg-gray-400">
+    <div className="flex h-screen w-screen flex-row bg-neutral-700">
       <SideMenu />
-      <section>
+      <section className="h-full w-full flex-1 flex-col justify-center">
         <ChatBox />
       </section>
     </div>
