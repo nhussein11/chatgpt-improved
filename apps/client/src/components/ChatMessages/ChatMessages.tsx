c22a330ec2cef92fbb074950afcd7115c751f6a9
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-23 23:01:28 -0300

    fix: updating rendering of messages in global state

--- apps/client/src/components/ChatMessages/ChatMessages.tsx
+++ apps/client/src/components/ChatMessages/ChatMessages.tsx
@@ -1,22 +1,20 @@
 import { RxAvatar } from 'react-icons/rx';
+import { Message, useChatsStore } from '../../Context/chatsStore';
 
 const ChatMessages = () => {
-  const messagesMock = [
-    "Hello, I'm a message",
-    'This is a test of the messages',
-  ];
+  const { messages } = useChatsStore();
 
   return (
     <div className="flex h-full w-full flex-grow items-center justify-center overflow-y-auto">
       <div className="flex h-24 w-2/4 flex-col items-center rounded-md bg-zinc-700 p-3 text-white">
-        {messagesMock.map((message) => (
-          <div className="flex h-24 w-full items-center justify-center">
-          <div className="flex h-full items-center ">
-            <RxAvatar className="justify-start text-3xl" />
-          </div>
-          <div className="flex h-full w-3/4 items-center justify-center text-xl">
-            {message}
-          </div>
+        {messages.map((message: Message, index: number) => (
+          <div className="flex h-24 w-full items-center justify-center" key={index}>
+            <div className="flex h-full items-center">
+              <RxAvatar className="justify-start text-3xl" />
+            </div>
+            <div className="flex h-full w-3/4 items-center justify-center text-xl">
+              {message.text}
+            </div>
           </div>
         ))}
       </div>
