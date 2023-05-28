e0b84decb620be7b0dbf47e53316f4c3aaba8c04
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-28 19:31:50 -0300

    fix: fixing scroll to bottom when new message is added

--- apps/client/src/components/ChatMessages/ChatMessages.tsx
+++ apps/client/src/components/ChatMessages/ChatMessages.tsx
@@ -1,18 +1,44 @@
 import { RxAvatar } from 'react-icons/rx';
+import {AiOutlineRobot} from 'react-icons/ai'
 import { Message, useChatsStore } from '../../Context/chatsStore';
+import { useEffect, useRef } from 'react';
 
 const ChatMessages = () => {
   const { messages } = useChatsStore();
+  const messagesRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    if (!messagesRef.current) return;
+    messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+  };
 
   return (
-    <div className="flex h-full w-full flex-grow items-center justify-center overflow-y-auto">
-      <div className="flex h-24 w-2/4 flex-col items-center rounded-md bg-zinc-700 p-3 text-white">
+    <div
+      className="border-red flex h-full w-full snap-y flex-col items-center justify-center overflow-auto"
+      ref={messagesRef}
+    >
+      <div className="flex w-full snap-end flex-col items-center rounded-md p-2 text-white">
         {messages.map((message: Message, index: number) => (
-          <div className="flex h-24 w-full items-center justify-center" key={index}>
-            <div className="flex h-full items-center">
-              <RxAvatar className="justify-start text-3xl" />
+          <div
+            className={`${message.user === 'gpt' ? 'bg-zinc-500' : ''} 
+            flex w-full items-center justify-center py-6 `}
+            key={index}
+          >
+            <div className="flex items-center px-3">
+              {message.user === 'gpt' ? (
+                <AiOutlineRobot className="justify-end text-3xl" />
+              ) : (
+                <RxAvatar className="justify-start text-3xl" />
+              )}
             </div>
-            <div className="flex h-full w-3/4 items-center justify-center text-xl">
+            <div
+              className="flex
+              w-3/4 items-center justify-center px-3 text-xl"
+            >
               {message.text}
             </div>
           </div>
