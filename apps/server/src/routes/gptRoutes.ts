7611b0d9846005c18c7d6933a42023fadd4c9cb5
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-05-29 15:49:15 -0300

    fix: testing welcome to server route to log keys

--- apps/server/src/routes/gptRoutes.ts
+++ apps/server/src/routes/gptRoutes.ts
@@ -4,12 +4,14 @@ import { getAutoCompletition } from '../services/gptService';
 const gptRouter = express.Router();
 
 gptRouter.get('/', (_req, res) => {
+  console.log('API_KEY', process.env.API_KEY);
+  console.log('API_URL', process.env.API_URL);
   res.send({ message: 'Welcome to server!' });
 });
 
 gptRouter.post('/create-completion', async (req, res) => {
   const { prompt } = req.body;
-  
+
   if (!prompt) {
     res.status(400).send({ error: 'Prompt is required' });
   }
@@ -17,8 +19,7 @@ gptRouter.post('/create-completion', async (req, res) => {
   try {
     const data = await getAutoCompletition(prompt);
     res.status(200).send({ data });
-  }
-  catch (error) {
+  } catch (error) {
     // TODO: check if this is the correct status code
     res.status(400).send({ error });
   }
