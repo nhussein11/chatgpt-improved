17aac5bb1a136ba7dd008bb317f3148bfe7a0021
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-06-01 11:31:07 -0300

    fix: updating client service with new endpoints

--- apps/client/src/services/createCompletion.ts
+++ apps/client/src/services/createCompletion.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
+import { isProductionEnv } from '../../../global-utils/environment';
 
 const createCompletion = async (prompt: string) => {
-  const response = await axios.post(
-    'http://localhost:3000/api/create-completion',
-    { prompt }
-  );
+  const endpoint = isProductionEnv
+    ? 'https://chatgpt-improved-api.onrender.com/api/create-completion'
+    : // https://chatgpt-improved-api.onrender.com/api
+      'http://localhost:3000/api/create-completion';
 
+  console.log('endpoint', endpoint);
+  const response = await axios.post(endpoint, { prompt });
+  console.log('response', response.data);
   return response.data;
 };
 
